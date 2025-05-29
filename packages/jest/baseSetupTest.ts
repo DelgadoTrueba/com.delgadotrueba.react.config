3fc4865f5755382d5117098c7e38f2a2de68748e
Delgado Trueba <jose.delgado@babel.es>
2025-05-29 17:33:58 +0200

    chore: install version

--- packages/jest/baseSetupTest.ts
+++ packages/jest/baseSetupTest.ts
@@ -1,6 +1,7 @@
 /// <reference types="@testing-library/jest-dom" />
-const { cleanup } = require('@testing-library/react');
 require('@testing-library/jest-dom');
+
+const { cleanup } = require('@testing-library/react');
 
 afterEach(() => {
   cleanup();
